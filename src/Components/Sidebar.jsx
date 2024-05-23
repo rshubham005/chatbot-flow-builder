2be4cd72d2877f704e97252075ee3d16f0966c96
Shubham Rathor <rathor.sr143.sr@gmail.com>
2024-05-23 16:06:59 +0530

    chat-bot done

---
+++ src/Components/Sidebar.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react'
+
+const Sidebar = () => {
+  const [showUsage, setShowUsage] = useState(true) // state to show helpful chat flow usage info
+
+  // Tell drag & drop usage of nodes to user on first load of the application
+  useEffect(() => {
+    setTimeout(() => {
+      setShowUsage(false)
+    }, 5000)
+  }, [showUsage])
+
+  // display show node usage information on first load of the application
+  const displayUsage = showUsage ? '' : 'none'
+
+  const onDragStart = (event, nodeType) => {
+    event.dataTransfer.setData('application/reactflow', nodeType)
+    event.dataTransfer.effectAllowed = 'move'
+  }
+
+  // You can expand upon adding new nodes by change the type from default
+  return (
+    <>
+      <aside>
+        <div
+          className="appnode"
+          onDragStart={(event) => onDragStart(event, 'default')}
+          draggable
+        >
+          <span
+            className="material-symbols-outlined"
+            style={{ paddingBottom: 5 }}
+          >
+            maps_ugc
+          </span>
+          Message
+        </div>
+      </aside>
+    </>
+  )
+}
+
+export default Sidebar
